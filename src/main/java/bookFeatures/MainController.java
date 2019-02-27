0561d24ef211777b31db8692df6545adec4d301c
TGLS <ThompsonStubbs@gmail.com>
2019-02-26 22:52:39 -0500

    Enabling Autoconfiguration

--- src/main/java/bookFeatures/MainController.java
+++ src/main/java/bookFeatures/MainController.java
@@ -1,6 +1,7 @@
 package bookFeatures;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Optional;
 
 @Controller
+@EnableAutoConfiguration
 public class MainController {
 
 	@Autowired

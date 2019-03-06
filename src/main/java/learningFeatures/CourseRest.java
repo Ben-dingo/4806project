17b8da0aefa5b7388a07595f9626927643aa80c5
benstpierre <cerulean8888@gmail.com>
2019-03-06 13:14:09 -0500

    added admin functionality -addCourseOutcome & addCatOutcome do not work as far as I can tell

--- src/main/java/learningFeatures/CourseRest.java
+++ src/main/java/learningFeatures/CourseRest.java
@@ -1,30 +1,37 @@
 package learningFeatures;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-
 @RestController
 public class CourseRest {
 
-    private ArrayList<Course> courses = new ArrayList<Course>();
+    @Autowired
+    CourseRepository courses;
+
+    @Autowired
+    CategoryRepository categories;
+
+    @Autowired
+    LearningObjectiveRepository objectives;
 
     @GetMapping("/newCourse")
     public String newCourse(@RequestParam(name="id") String id) {
-        courses.add(new Course(id));
+        courses.save(new Course(id));
         return "Success";
     }
 
-    @GetMapping("/newOutcome")
-    public String newOutcome(@RequestParam(name="id") String id, @RequestParam(name="name") String name, @RequestParam(name="desc") String desc) {
+    @GetMapping("/addCourseOutcome")
+    public String addCourseOutcome(@RequestParam(name="id") String id, @RequestParam(name="name") String name) {
 
-        for (Course courses: courses) {
+        for (Course courses: courses.findAll()) {
             if(courses.getName().equals(id)) {
-                LearningObjective newLearn = new LearningObjective(name,desc);
-                courses.addObjective(newLearn);
-                return "Success";
+                for (LearningObjective objectives: objectives.findByName(name)) {
+                    courses.addObjective(objectives);
+                    return "Success";
+                }
             }
         }
         return "Failure";
@@ -32,7 +39,7 @@ public class CourseRest {
 
     @GetMapping("/viewCourse")
     public String viewCourse(@RequestParam(name="id") String id) {
-        for (Course courses : courses) {
+        for (Course courses : courses.findAll()) {
             if(courses.getName().equals(id)) {
                 return courses.toString();
             }
@@ -43,8 +50,73 @@ public class CourseRest {
     @GetMapping("/viewCourses")
     public String viewCourse() {
         String returning = "";
-        for (Course courses : courses) {
+        for (Course courses : courses.findAll()) {
                 returning += courses.toString() + " ";
+        }
+        return returning;
+    }
+
+    @GetMapping("/newCat")
+    public String newCategory(@RequestParam(name="id") String id) {
+        categories.save(new Category(id));
+        return "Success";
+    }
+
+    @GetMapping("/addCatOutcome")
+    public String addCatOutcome(@RequestParam(name="id") String id, @RequestParam(name="name") String name) {
+
+        for (Category categories: categories.findAll()) {
+            if(categories.getName().equals(id)) {
+                for (LearningObjective objectives: objectives.findByName(name)) {
+                    categories.addObjective(objectives);
+                    return "Success";
+                }
+            }
+        }
+        return "Failure";
+    }
+
+    @GetMapping("/viewCat")
+    public String viewCat(@RequestParam(name="id") String id) {
+        for (Category categories: categories.findAll()) {
+            if(categories.getName().equals(id)) {
+                return categories.toString();
+            }
+        }
+        return "Nothing";
+    }
+
+    @GetMapping("/viewCats")
+    public String viewCats() {
+        String returning = "";
+        for (Category categories: categories.findAll()) {
+            returning += categories.toString() + " ";
+        }
+        return returning;
+    }
+
+
+    @GetMapping("/newObjective")
+    public String newObjective(@RequestParam(name="id") String name, @RequestParam(name="name") String desc) {
+        objectives.save(new LearningObjective(name,desc));
+        return "Success";
+    }
+
+    @GetMapping("/viewObjective")
+    public String viewObjective(@RequestParam(name="id") String id) {
+        for (LearningObjective objectives: objectives.findAll()) {
+            if(objectives.getName().equals(id)) {
+                return objectives.toString();
+            }
+        }
+        return "Nothing";
+    }
+
+    @GetMapping("/viewObjectives")
+    public String viewObjectives() {
+        String returning = "";
+        for (LearningObjective objectives: objectives.findAll()) {
+            returning += objectives.toString() + " ";
         }
         return returning;
     }

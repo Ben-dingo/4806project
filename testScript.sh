af5c4cf3c70a1fed829a2e0199e838a8a4ca445a
TGLS <ThompsonStubbs@gmail.com>
2019-03-15 10:49:32 -0400

    Cleaned up httpTest into multiple tests.

--- testScript.sh
+++ testScript.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Negotiate with Demons", "description": "Student is capable of negotiating favourable terms with demons."}' http://random4806.herokuapp.com/objective
 curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Identifying Items", "description": "Student is capable of identifying magical items by touch, spell, and eBay valuation."}' http://random4806.herokuapp.com/objective
 curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Imperative Programming", "description": "Student is capable of writing with if statements and loops."}' http://random4806.herokuapp.com/objective
 curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Pointer Math", "description": "Student is capable of getting to the correct locations in memory given a type definition."}' http://random4806.herokuapp.com/objective
-curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Requirements Engineering", "description": "Student is capable of writing well-defined and clear requirments."}' http://random4806.herokuapp.com/objective
+curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Requirements Engineering", "description": "Student is capable of writing well-defined and clear requirements."}' http://random4806.herokuapp.com/objective
 
 curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Introduction to Magic"}' http://random4806.herokuapp.com/course
 curl -i -X POST -H "Content-Type:application/json" -d '{"name": "Introduction to Programming"}' http://random4806.herokuapp.com/course
